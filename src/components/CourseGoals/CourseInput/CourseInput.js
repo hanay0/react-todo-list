af02dcb32b831f8ece60e4448a0b04c365d5576c
ahmed hanafy <ahanafy90@yahoo.com>
2023-05-16 23:44:12 +0300

    adding dynamic styles for input whether it's empty or not

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,35 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+      // resetting the styles if the input is not an empty one
+      (event.target.value).trim().length > 0 ? setIsValid(true) : setIsValid(false);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false) // update the state to be false , and due to that boolean we will add dynamic styles
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input
+            type="text"
+            onChange={goalInputChangeHandler}
+            style={{
+              border: !isValid ? '2px solid red': '2px solid #ccc',
+              backgroundColor: !isValid ? 'salmon' : 'transparent'
+            }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
